03db0fa8f3fe2bec9dc8798cb41bb2adb0c0e1b3
sundehakon <142574795+sundehakon@users.noreply.github.com>
2023-10-20 14:48:19 +0200

    added ascii art and fixed wrong user input message

--- Program.cs
+++ Program.cs
@@ -1,25 +1,79 @@
 ﻿Random rnd = new Random();
 int randomNumber = rnd.Next(1,101);
+Console.WriteLine(randomNumber);
 
-Console.Write("Choose a number between 1-100: ");
-int userInput = int.Parse(Console.ReadLine());
+string asciiArt = @"
+                      _                                         _                                            
+                     | |                                       | |                                           
+  _ __ __ _ _ __   __| | ___  _ __ ___    _ __  _   _ _ __ ___ | |__   ___ _ __    __ _  __ _ _ __ ___   ___ 
+ | '__/ _` | '_ \ / _` |/ _ \| '_ ` _ \  | '_ \| | | | '_ ` _ \| '_ \ / _ \ '__|  / _` |/ _` | '_ ` _ \ / _ \
+ | | | (_| | | | | (_| | (_) | | | | | | | | | | |_| | | | | | | |_) |  __/ |    | (_| | (_| | | | | | |  __/
+ |_|  \__,_|_| |_|\__,_|\___/|_| |_| |_| |_| |_|\__,_|_| |_| |_|_.__/ \___|_|     \__, |\__,_|_| |_| |_|\___|
+                                                                                   __/ |                     
+                                                                                  |___/                      
+";
+Console.WriteLine(asciiArt);
+
+string gameOver = @"
+                                                   
+                                                   
+   __ _  __ _ _ __ ___   ___    _____   _____ _ __ 
+  / _` |/ _` | '_ ` _ \ / _ \  / _ \ \ / / _ \ '__|
+ | (_| | (_| | | | | | |  __/ | (_) \ V /  __/ |   
+  \__, |\__,_|_| |_| |_|\___|  \___/ \_/ \___|_|   
+   __/ |                                           
+  |___/                                            
+";
+
+string winner = @"
+                                          
+                                          
+  _   _  ___  _   _  __      _____  _ __  
+ | | | |/ _ \| | | | \ \ /\ / / _ \| '_ \ 
+ | |_| | (_) | |_| |  \ V  V / (_) | | | |
+  \__, |\___/ \__,_|   \_/\_/ \___/|_| |_|
+   __/ |                                  
+  |___/                                   
+";
+
+int userInput = 0;
+
+while (true)
+{
+    Console.Write("Choose a number between 1-100: ");
+    userInput = int.Parse(Console.ReadLine());
+
+    if (userInput >= 1 && userInput <= 100)
+    {
+        break;
+    }
+
+    Console.WriteLine("Invalid input, please choose a number between 1 and 100.");
+}
 
 int difference = randomNumber - userInput;
-
-if (userInput == randomNumber) 
+int positiveDifference = userInput - randomNumber;
+try
 {
-    Console.WriteLine("Good job, you're correct!");
-}
-else if (userInput > randomNumber)
+    if (userInput == randomNumber) 
+    {
+        Console.WriteLine(winner);
+    }
+    else if (userInput > randomNumber)
+    {   
+        Console.WriteLine(gameOver);
+        Console.WriteLine($"You we're off by: {positiveDifference}");
+    }
+    else if (userInput < randomNumber)
+    {
+        Console.WriteLine(gameOver);
+        Console.WriteLine($"You we're off by: {difference}");
+    }
+    else 
+    {
+        Console.WriteLine("You're incorrect, try again!");
+    }
+} catch (Exception e)
 {
-    Console.WriteLine("Wayyyy too much, try again!");
-}
-else if (userInput < randomNumber)
-{
-    Console.WriteLine("Your guess is too low, please try again!");
-    Console.WriteLine($"You we're off by: {difference}");
-}
-else 
-{
-    Console.WriteLine("You're incorrect, try again!");
+    Console.WriteLine(e.Message);
 }
