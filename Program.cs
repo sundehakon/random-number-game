dd82cb9956078789b2ae13628419e77ff5cdb747
sundehakon <142574795+sundehakon@users.noreply.github.com>
2023-10-20 14:22:43 +0200

    added results, a difference score and fixed issues

--- Program.cs
+++ Program.cs
@@ -1,5 +1,25 @@
 ﻿Random rnd = new Random();
-int randomNumber = rnd.Next(1,100);
+int randomNumber = rnd.Next(1,101);
 
 Console.Write("Choose a number between 1-100: ");
-string userInput = Console.ReadLine();
+int userInput = int.Parse(Console.ReadLine());
+
+int difference = randomNumber - userInput;
+
+if (userInput == randomNumber) 
+{
+    Console.WriteLine("Good job, you're correct!");
+}
+else if (userInput > randomNumber)
+{
+    Console.WriteLine("Wayyyy too much, try again!");
+}
+else if (userInput < randomNumber)
+{
+    Console.WriteLine("Your guess is too low, please try again!");
+    Console.WriteLine($"You we're off by: {difference}");
+}
+else 
+{
+    Console.WriteLine("You're incorrect, try again!");
+}
